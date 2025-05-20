dbfa15fc7b2065bf47cf543c65db99195b23a1a9
RenLapislazuli <andrey.tsoy.07@mail.ru>
2025-05-20 12:54:07 +0300

    3

--- cinemabot/main.py
+++ cinemabot/main.py
@@ -4,11 +4,12 @@ import asyncio
 # import aiogram
 # from aiogram.types import BotCommand
 
-from cinemabot import dependencies
+from cinemabot import dependencies, background
 
 async def main() -> None:
     dp, bot = await dependencies.get_dispatcher_and_bot()
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    background.keep_alive()
     asyncio.run(main())
